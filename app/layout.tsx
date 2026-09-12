ef334e9b8e4d339a6f018ab1924bcc00ba5a8e07
Tivalsdeveloper <tivalsdeveloper@gmail.com>
2026-09-12 23:48:52 +0200

    Keep only AI rewarded ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,15 +34,6 @@ export default function RootLayout({
       </head>
       <body className="antialiased">
         {children}
-        <Script
-          src="https://quge5.com/88/tag.min.js"
-          data-zone="279691"
-          data-cfasync="false"
-          strategy="afterInteractive"
-        />
-        <Script id="monetag-zone-11786694" strategy="afterInteractive">
-          {`(function(s){s.dataset.zone='11786694';s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`}
-        </Script>
         <Script id="ai-reward-gate" strategy="afterInteractive">
           {`
 (function () {
